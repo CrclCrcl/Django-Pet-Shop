3c2a6323a88673d4a2286e48675e8806d39621a2
Tomasz <tomaszkaramon3@gmail.com>
2023-05-01 21:50:44 +0200

    Bugs Fixed
    *Fixed:
    -fixed bugs related to displaying the product view
    *Corrected:
    -Aesthetics related to the appearance of the page

--- src/Petshop/Items/views.py
+++ src/Petshop/Items/views.py
@@ -1,10 +1,22 @@
-from django.shortcuts import render, get_object_or_404
 
-from .models import Item
 # Create your views here.
 
-def detail(request, pk):
-    item = get_object_or_404(Item,pk=pk)
-    return render(request, 'Item/details.html',{
-        'item' : item
-    })
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse
+from .models import Item
+
+def index(request):
+    items = Item.objects.all()
+    context = {
+        'items': items
+    }
+    return render(request, 'index.html', context)
+
+def handler404(request, exception):
+    return render(request, '404.html', status=404)
+
+
+def item_detail(request, pk):
+    item = get_object_or_404(Item, pk=pk)
+    context = {'item': item}
+    return render(request, 'item_detail.html', {'item': item})
--- src/Petshop/Petshop/urls.py
+++ src/Petshop/Petshop/urls.py
@@ -22,4 +22,6 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('',index, name="index"),
     path('admin/', admin.site.urls),
-]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    path('items/', include('Items.urls')),
+
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
